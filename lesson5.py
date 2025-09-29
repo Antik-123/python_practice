60f07b7c93f8238f5bd35d523a19040de5250a5c
Antik-123 <antikmahmud99@gmail.com>
2025-09-30 04:22:36 +0600

    second commit

---
+++ lesson5.py
@@ -0,0 +1,55 @@
+#Arithemetic operators
+import math
+
+print(math.pi)
+print(math.e)
+
+friends = 5
+
+# friends = friends + 1
+
+# friends-= 2
+
+# friends**= 2
+
+#built in math related function
+
+# x = 3.14
+# y = -4
+# z = 5
+
+# result = round(x)
+# result = abs(y)
+# result =  pow(4, 6)
+
+# result = max(x,y,z)
+# result = min(x,y,z)
+
+
+x = 9.9
+
+# result = math.sqrt(x)
+# result =  math.ceil(x)
+# result =  math.floor(x)
+
+
+# print(result)
+
+# print(friends)
+
+# print(result)
+
+
+
+# radius = float(input('enter the radius of a circle : '))
+
+# circumference = 2 * math.pi *radius
+
+# print(f"The circumference is : {circumference}")
+
+
+
+radius = float(input("enter the radius of the area : "))
+area =  math.pi * pow(radius, 2)
+
+print(f"the area is : {area}")
