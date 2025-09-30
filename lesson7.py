a38210522b5c9270130736a95da6f8162728e1b2
Antik-123 <antikmahmud99@gmail.com>
2025-10-01 00:37:44 +0600

    fifth commit

---
+++ lesson7.py
@@ -0,0 +1,8 @@
+#python calculator
+
+operator = input("enter an operator (+ - * /) : ")
+
+num1 = int(input("enter the 1st number: "))
+num2 = int(input("enter the 2nd number: "))
+
+print(num1 + num2)
---
+++ lesson8.py
@@ -0,0 +1,29 @@
+#objected oriented programming
+
+# 1.Basic Class and object 
+# >>Create Car class with attributes like brand and model. Then create an instance of this class 
+# 2.Class Method and Self 
+# 3.Inherirence
+# 4.Encapsulation
+# 5.Class Variables 
+# 6.Static Method 
+# 7.Property Decorators 
+# 8.Class Inheritence and is isinstance() function
+# 9.Multiple Inheritance 
+
+
+# class Car:
+#  def __init__(self, brand, model):
+#   self.model = model
+#   self.brand = brand
+
+
+
+# my_car = Car("tesla", "corola")
+
+
+# print(my_car.brand)
+
+
+
+
