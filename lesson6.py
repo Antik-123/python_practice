12cc96cf4b8ceb11d1a4bb62d6a858dfbffe5683
Antik-123 <antikmahmud99@gmail.com>
2025-09-30 23:41:48 +0600

    fourth commit

---
+++ lesson6.py
@@ -0,0 +1,40 @@
+#if = Do some code only If some conditon is True 
+#     Else do something else
+
+
+# age = int(input('enter your age :'))
+
+# if age>= 100:
+#   print("you are too old to signup")
+# elif age >= 18:
+#   print("you are now signed up")
+# elif age < 0:
+#   print("you haven't been born yet")
+
+# else:
+#   print("you must be 18+ to sign up")
+
+
+# respond = input("would you like food? (y/n): ")
+
+# if respond == "Y":
+#   print("have some food")
+# else:
+#   print("No food for you")
+
+
+# name = input("enter your name: ")
+
+# if name == "":
+#   print("you did not type in your name")
+# else:
+#   print(f"Hello {name}")
+
+
+
+for_sale = True
+
+if for_sale:
+  print('this item is for sale')
+else:
+  print("this item is not for sale")
